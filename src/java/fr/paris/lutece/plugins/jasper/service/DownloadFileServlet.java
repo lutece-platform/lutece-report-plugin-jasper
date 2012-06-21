46b4b29ca90e47e5c65fdc518796dd1d10a536fc
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2012-06-21 09:26:09 +0000

    JASPER-18 :Add servlet for downloadin file
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-jasper@41763 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/jasper/service/DownloadFileServlet.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2002-2011, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.jasper.service;
+
+import fr.paris.lutece.portal.service.util.AppLogService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * DownloadFileServlet
+ *
+ *
+ */
+public class DownloadFileServlet extends HttpServlet
+{
+    private static final long serialVersionUID = -6564244054015195801L;
+
+    public DownloadFileServlet(  )
+    {
+        super(  );
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException the servlet Exception
+     * @throws IOException the io exception
+     */
+    protected void processRequest( HttpServletRequest request, HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        try
+        {
+            byte[] fileContent = JasperFileLinkService.exportFile( request );
+            String strFileName = JasperFileLinkService.getFileName( request );
+
+            if ( fileContent != null )
+            {
+                //the header and also the names set by which user will be prompted to save
+                response.setHeader( "Content-Disposition", "attachment;filename=\"" + strFileName + "\"" );
+
+                String strMimetype = getServletContext(  ).getMimeType( strFileName );
+                response.setContentType( ( strMimetype != null ) ? strMimetype : "application/octet-stream" );
+                response.setHeader( "Cache-Control", "must-revalidate" );
+                response.getOutputStream(  ).write( fileContent );
+            }
+        }
+        catch ( Exception e )
+        {
+            AppLogService.error( e.getMessage(  ), e );
+        }
+        finally
+        {
+            response.getOutputStream(  ).flush(  );
+        }
+    }
+
+    /** Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+         * @throws ServletException the servlet Exception
+         * @throws IOException the io exception
+     */
+    protected void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        processRequest( request, response );
+    }
+
+    /** Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+         * @throws ServletException the servlet Exception
+         * @throws IOException the io exception
+     */
+    protected void doPost( HttpServletRequest request, HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        processRequest( request, response );
+    }
+
+    /** Returns a short description of the servlet.
+         * @return message
+     */
+    public String getServletInfo(  )
+    {
+        return "Servlet downloadFile content";
+    }
+}
